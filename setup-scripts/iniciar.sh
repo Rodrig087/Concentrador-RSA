87fc6289f11b8fa48dd444d6f8761744fae28b71
Rodrig087 <miltonho@hotmail.com>
2023-09-27 20:44:59 +0000

    Se actualizaron ls setup-script para incluir una variable de entorno en el nombre del usuario

--- setup-scripts/iniciar.sh
+++ setup-scripts/iniciar.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Copiar el directorio "Configuracion" a la ruta /home/rsa/
-cp -r configuracion /home/rsa/
+cp -r configuracion /home/"$USER"/
 
 # Crea los directorios necesarios
-mkdir -p /home/rsa/ejecutables
-mkdir -p /home/rsa/log-files
-mkdir -p /home/rsa/tmp
-mkdir -p /home/rsa/resultados/eventos-detectados
+mkdir -p /home/"$USER"/ejecutables
+mkdir -p /home/"$USER"/log-files
+mkdir -p /home/"$USER"/tmp
+mkdir -p /home/"$USER"/resultados/eventos-detectados
 
 # Compila todos los programas escritos en C 
 sh setup-scripts/compilar.sh
 
 # Copia los scripts de Python a la carpeta /home/rsa/ejecutables
-cp /home/rsa/Acelerografo-RSA/programas/*.py /home/rsa/ejecutables/
+cp /home/"$USER"/Concentrador-RSA/programas/*.py /home/"$USER"/ejecutables/
